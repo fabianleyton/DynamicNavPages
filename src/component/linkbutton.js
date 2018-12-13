d9e0c4e689cf7056511f1c15b5a35c99e8110bff
Fabian Leyton <fabianlock@hotmail.com>
2018-12-12 23:05:36 -0500

    best design card component aplied

--- src/component/linkbutton.js
+++ src/component/linkbutton.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 function LinkButton(props){
     return (
-        <button 
-            className="btn btn-danger" 
-            key={props.button.id}
-            onClick = {()=>props.handleClick(props.button.subItems, (props.button.url!=undefined && props.button.url.length > 0) ? props.button.url : "")}
-        >
-                {props.button.name}
-        </button>
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.button.name}</h5>
+                    <p className="card-text">{props.button.description}</p>
+                    <button 
+                        className="btn btn-danger" 
+                        key={props.button.id}
+                        onClick = {()=>props.handleClick(props.button.subItems, (props.button.url!=undefined && props.button.url.length > 0) ? props.button.url : "")}
+                    >
+                            Acceder
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/component/listbuttons.js
+++ src/component/listbuttons.js
@@ -6,7 +6,7 @@ function ListButtons(props){
         <div className="col-sm-10 col-md-10 offset-sm-1 offset-sm-1 row">
         {
             props.listbuttons.map((item)=>{
-                return <div className="col-md-3"><LinkButton key={item.id} button={item} handleClick={props.handleClick}></LinkButton></div>
+                return <div key={item.id} className="col-md-3"><LinkButton key={item.id} button={item} handleClick={props.handleClick}></LinkButton></div>
             })
         }
         </div>
