2817617f888436b9f22541de215b22bfaacfdaf9
Fabian Leyton <fabianlock@hotmail.com>
2018-12-12 23:19:38 -0500

    fix

--- src/component/containerweb.js
+++ src/component/containerweb.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ContainerWeb(props){
     return(
         <div>
-            <iframe width={650} height={500} src={props.url} frameBorder={0}></iframe>
+            <iframe width={650} height={500} src={props.src} frameBorder={0}></iframe>
         </div>
     )
 }
