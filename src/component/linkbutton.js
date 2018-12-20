1e4dee2624dad6673a329db81108516db29ff185
Fabian Leyton <fabianlock@hotmail.com>
2018-12-19 22:27:00 -0500

    minor fix - add transitiongroup

--- src/component/linkbutton.js
+++ src/component/linkbutton.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-
-function LinkButton(props){
+function LinkButton(props) {
     return (
         <div>
-            
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{props.button.name}</h5>
-                    <p className="card-text">{props.button.description}</p>
-                    <button 
-                        className="btn btn-danger" 
-                        key={props.button.id}
-                        onClick = {()=>props.handleClick(props.button.subItems, (props.button.url!=undefined && props.button.url.length > 0) ? props.button.url : "")}
-                    >
+            <CSSTransition 
+                in={true}
+                timeout={1000}
+                unmountOnExit
+            >
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.button.name}</h5>
+                        <p className="card-text">{props.button.description}</p>
+                        <button
+                            className="btn btn-danger"
+                            key={props.button.id}
+                            onClick={() => props.handleClick(props.button.subItems, (props.button.url != undefined && props.button.url.length > 0) ? props.button.url : "")}
+                        >
                             Acceder
                     </button>
+                    </div>
                 </div>
-            </div>
+            </CSSTransition>
         </div>
     )
 }
