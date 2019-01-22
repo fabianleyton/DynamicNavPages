e6dcd06d8401bd1b5e426a44eeb25232e9e4430d
Fabian Leyton <fabianlock@hotmail.com>
2019-01-21 23:26:28 -0500

    change component to container

--- src/component/linkbutton.js
+++ src/component/linkbutton.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function LinkButton(props) {
-    return (
-        <div>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{props.button.name}</h5>
-                    <p className="card-text">{props.button.description}</p>
-                    <button
-                        className="btn btn-danger"
-                        key={props.button.id}
-                        onClick={() => props.handleClick(props.button.subItems, (props.button.url != undefined && props.button.url.length > 0) ? props.button.url : "")}
-                    >
-                        Acceder
-                </button>
+class LinkButton extends Component{
+    render(){
+        return (
+            <div>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.button.name}</h5>
+                        <p className="card-text">{this.props.button.description}</p>
+                        <button
+                            className="btn btn-danger"
+                            key={this.props.button.id}
+                            onClick={() => this.props.handleClick(this.props.button.subItems, (this.props.button.url != undefined && this.props.button.url.length > 0) ? this.props.button.url : "")}
+                        >
+                            Acceder
+                    </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+//function LinkButton(props) {
+    
+//}
 
 export default LinkButton
